f8c261d0c0a65d7cc6986b35f8b15a35bcf49121
rafaelfachinelli <rafael.fachinelli@fatec.sp.gov.br>
2020-10-15 20:04:56 -0300

    server finished

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,17 @@
 import express from 'express';
+import 'express-async-errors';
+import path from 'path';
+
+import './database/connection';
+
+import routes from './routes';
+import errorHandler from './errors/handler';
 
 const app = express();
 
 app.use(express.json());
-
-app.get('/users', (request, response) => {
-  return response.json({"message": "Hello World"});
-});
+app.use(routes);
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
+app.use(errorHandler);
 
 app.listen(3333);
